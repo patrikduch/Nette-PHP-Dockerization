e632fb1e26a1bd4a32db5f4c94566c1f3cc0a905
patrikduch <duchpatrik@icloud.com>
2020-11-15 03:49:36 +0100

    AJAX Nette integration

--- webapp/app/Presenters/HomepagePresenter.php
+++ webapp/app/Presenters/HomepagePresenter.php
@@ -10,14 +10,30 @@ use Nette;
 final class HomepagePresenter extends Nette\Application\UI\Presenter
 {
 
-    private $database;
+    private $clicked = false;
 
+    private $database;
 
     public function __construct(Nette\Database\Context $database) {
         $this->database = $database;
     }
 
+
+    /* Signal */
+    public function handleChangeFoo() {
+        $this->clicked = !($this->clicked);
+
+        if ($this->isAjax()) {
+            $this->redrawControl('clicked_area'); // invalid snippet 'clicked_area'
+        }
+    }
+
     public function renderDefault() {
         $this->template->projectDetail = $this->database->table('ProjectDetail')->limit(1);
+        $this->template->clicked = $this->clicked;
     }
+
+
+
+
 }
