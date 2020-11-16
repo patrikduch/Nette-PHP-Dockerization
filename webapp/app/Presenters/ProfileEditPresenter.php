cc1e9ecbead9597749a2bb3208949230caf7f8c1
patrikduch <duchpatrik@icloud.com>
2020-11-16 01:59:14 +0100

    Latte: profile edit page

---
+++ webapp/app/Presenters/ProfileEditPresenter.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Presenters;
+
+use App\Presenters\BasePresenter;
+
+final class ProfileEditPresenter extends BasePresenter {
+
+
+    public function renderDefault() {
+
+    }
+}
